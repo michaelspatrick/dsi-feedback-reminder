6dee595fdb9554caef82283fd6d04bbd6f79ff30
Mike Patrick <45206934+michaelspatrick@users.noreply.github.com>
2025-07-16 15:04:35 -0400

    Update dsi-feedback-reminder.php

--- dsi-feedback-reminder.php
+++ dsi-feedback-reminder.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: DSI Seminar Feedback Reminder
 Description: Sends seminar attendees an email requesting feedback one hour after the seminar ends.
-Version: 1.0
+Version: 1.1
 Author: Michael Patrick
 */
 
@@ -44,10 +44,13 @@ function dsi_send_feedback_reminders() {
     $query = new WP_Query($args);
     if (!$query->have_posts()) return;
 
+    $from_email = get_option('dsi_feedback_from_email', 'newsletter@dragonsociety.com');
+    $subject    = get_option('dsi_feedback_subject', "We'd love your feedback on the seminar!");
+    $body_raw   = get_option('dsi_feedback_message', "<p>Thank you for attending our seminar. We'd love your feedback.</p><p><a href='https://feedback.dragonsociety.com'>Click here</a> to share your thoughts.</p><p>Check out our <a href='https://www.patreon.com/c/DragonSocietyInternational'>Patreon</a> for exclusive content.</p>");
+
     foreach ($query->posts as $event) {
         $event_id = $event->ID;
         $product_ids = dsi_get_event_product_ids($event_id);
-
         if (empty($product_ids)) continue;
 
         $orders = wc_get_orders([
@@ -57,34 +60,30 @@ function dsi_send_feedback_reminders() {
         ]);
 
         foreach ($orders as $order) {
-            foreach ($order->get_items() as $item_id => $item) {
-                $product_id = $item->get_product_id();
-                if (!in_array($product_id, $product_ids)) continue;
+            foreach ($order->get_items() as $item) {
+                if (!in_array($item->get_product_id(), $product_ids)) continue;
 
                 $meta_key = '_dsi_feedback_sent_' . $event_id;
-                if ($order->get_meta($meta_key)) continue; // Already sent for this event
+                if ($order->get_meta($meta_key)) continue;
 
                 $email = $order->get_billing_email();
                 $name = $order->get_billing_first_name();
-
-                $subject = "We'd love your feedback on the seminar!";
-                $body = "<p>Hi {$name},</p>
-                         <p>Thank you for attending our seminar. We'd love to hear your thoughts so we continue to improve.</p>
-                         <p>Your feedback ensures we customize the seminar experience to be the best it can possibly be.</p>
-                         <p><a href='https://feedback.dragonsociety.com' target='_blank'>Click here to give your feedback</a>.</p>
-                         <p>Also, check out exclusive content on our <a href='https://www.patreon.com/c/DragonSocietyInternational' target='_blank'>Patreon page</a>.</p>
-                         <p>- Dragon Society International</p>";
+                $body = str_replace('{name}', esc_html($name), $body_raw);
 
                 wp_mail($email, $subject, $body, [
-                    'Content-Type: text/html; charset=UTF-8'
+                    'Content-Type: text/html; charset=UTF-8',
+                    "From: $from_email"
                 ]);
 
                 $order->update_meta_data($meta_key, 1);
                 $order->save();
             }
         }
-        // Send an email to Mike so he knows it was sent
-        wp_mail("toritejutsu@gmail.com", $subject, $body, [ 'Content-Type: text/html; charset=UTF-8' ]);  
+
+        wp_mail("toritejutsu@gmail.com", $subject, $body_raw, [
+            'Content-Type: text/html; charset=UTF-8',
+            "From: $from_email"
+        ]);
     }
 }
 
@@ -101,3 +100,49 @@ function dsi_get_event_product_ids($event_id) {
     return $ids;
 }
 
+// Admin settings page
+add_action('admin_menu', function() {
+    add_options_page(
+        'DSI Feedback Settings',
+        'DSI Feedback Settings',
+        'manage_options',
+        'dsi-feedback-settings',
+        'dsi_feedback_settings_page'
+    );
+});
+
+add_action('admin_init', function() {
+    register_setting('dsi_feedback_options', 'dsi_feedback_from_email');
+    register_setting('dsi_feedback_options', 'dsi_feedback_subject');
+    register_setting('dsi_feedback_options', 'dsi_feedback_message');
+});
+
+function dsi_feedback_settings_page() {
+    ?>
+    <div class="wrap">
+        <h1>DSI Feedback Email Settings</h1>
+        <form method="post" action="options.php">
+            <?php settings_fields('dsi_feedback_options'); ?>
+            <?php do_settings_sections('dsi_feedback_options'); ?>
+            <table class="form-table">
+                <tr>
+                    <th scope="row"><label for="dsi_feedback_from_email">Sender Email</label></th>
+                    <td><input type="email" name="dsi_feedback_from_email" value="<?php echo esc_attr(get_option('dsi_feedback_from_email')); ?>" class="regular-text" /></td>
+                </tr>
+                <tr>
+                    <th scope="row"><label for="dsi_feedback_subject">Email Subject</label></th>
+                    <td><input type="text" name="dsi_feedback_subject" value="<?php echo esc_attr(get_option('dsi_feedback_subject')); ?>" class="regular-text" /></td>
+                </tr>
+                <tr>
+                    <th scope="row"><label for="dsi_feedback_message">Email Message (HTML)</label></th>
+                    <td>
+                        <textarea name="dsi_feedback_message" rows="10" class="large-text"><?php echo esc_textarea(get_option('dsi_feedback_message')); ?></textarea>
+                        <p class="description">Use <code>{name}</code> to insert attendee’s first name.</p>
+                    </td>
+                </tr>
+            </table>
+            <?php submit_button(); ?>
+        </form>
+    </div>
+    <?php
+}
